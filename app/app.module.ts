fea379584b67d66e6dd24a5552ef583b2f2a64c1
KaziNeha <kazi.neha601@gmail.com>
2021-04-03 21:11:06 +0530

    first commit

---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClaimInsuranceComponent } from './claim-insurance/claim-insurance.component';
+import { ApplyInsuranceComponent } from './apply-insurance/apply-insurance.component';
+import { ApplyBidComponent } from './apply-bid/apply-bid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClaimInsuranceComponent,
+    ApplyInsuranceComponent,
+    ApplyBidComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
