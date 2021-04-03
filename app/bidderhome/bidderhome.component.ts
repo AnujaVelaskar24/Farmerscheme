4d5d48138b2cf907553314d3852e01a39d151cf5
AnujaVelaskar24 <AnujaVelaskar@lntinfotech.com>
2021-04-03 19:16:35 +0530

    completed bidder and farmer home page

---
+++ app/bidderhome/bidderhome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidderhome',
+  templateUrl: './bidderhome.component.html',
+  styleUrls: ['./bidderhome.component.css']
+})
+export class BidderhomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
