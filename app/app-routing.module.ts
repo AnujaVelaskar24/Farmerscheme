76f7d7c6ca3d1fbfecdd125030add1610359a6c7
AnujaVelaskar24 <AnujaVelaskar@lntinfotech.com>
2021-04-04 22:26:47 +0530

    Routing started

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { FarmerhomeComponent} from './farmerhome/farmerhome.component';
+import { ClaimInsuranceComponent } from './claim-insurance/claim-insurance.component';
+import { BidderhomeComponent } from './bidderhome/bidderhome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FarmerhomeComponent},
+  {path: 'Home', component: HomeComponent},
+  {path: 'Farmerhome', component: FarmerhomeComponent},
+  {path: 'ClaimInsurance', component: ClaimInsuranceComponent},
+  {path: 'BidderHome', component: BidderhomeComponent},
+  // {path: '**', component: ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
