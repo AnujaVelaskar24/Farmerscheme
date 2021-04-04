f44ce621bc26587c34c5cb1ab9b6825ca6720e1e
AnujaVelaskar24 <AnujaVelaskar@lntinfotech.com>
2021-04-04 23:57:05 +0530

    routing

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,13 +4,41 @@ import {HomeComponent} from './home/home.component';
 import { FarmerhomeComponent} from './farmerhome/farmerhome.component';
 import { ClaimInsuranceComponent } from './claim-insurance/claim-insurance.component';
 import { BidderhomeComponent } from './bidderhome/bidderhome.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {MarketplaceComponent} from './marketplace/marketplace.component';
+import {FarmerTransactionHistoryComponent} from './farmer-transaction-history/farmer-transaction-history.component';
+import {BidderTransactionHistoryComponent} from './bidder-transaction-history/bidder-transaction-history.component';
+import {BidRequestComponent} from './bid-request/bid-request.component';
+import {ApplyInsuranceComponent} from './apply-insurance/apply-insurance.component';
+import {AdminhomeComponent} from './adminhome/adminhome.component';
+import {AdminLoginComponent} from './admin-login/admin-login.component';
+import {AdminCropApprovalComponent} from './admin-crop-approval/admin-crop-approval.component';
+import {AdminApprovalComponent} from './admin-approval/admin-approval.component';
+import {AdminAccountApprovalComponent} from './admin-account-approval/admin-account-approval.component';
+import {ApplyBidComponent} from './apply-bid/apply-bid.component';
+
 
 const routes: Routes = [
-  {path: '', component: FarmerhomeComponent},
+  {path: '', component: AdminhomeComponent},
   {path: 'Home', component: HomeComponent},
   {path: 'Farmerhome', component: FarmerhomeComponent},
   {path: 'ClaimInsurance', component: ClaimInsuranceComponent},
   {path: 'BidderHome', component: BidderhomeComponent},
+  {path: 'LoginComponent', component: LoginComponent},
+  {path: 'RegisterComponent', component: RegisterComponent},
+  {path: 'MarketplaceComponent', component: MarketplaceComponent},
+  {path: 'BidderTransactionHistoryComponent', component: BidderTransactionHistoryComponent},
+  {path: 'FarmerTransactionHistoryComponent', component: FarmerTransactionHistoryComponent},
+  {path: 'BidRequestComponent', component: BidRequestComponent},
+  {path: 'ApplyInsuranceComponent', component: ApplyInsuranceComponent},
+  {path: 'ApplyBidComponent', component: ApplyBidComponent},
+  {path: 'AdminhomeComponent', component: AdminhomeComponent},
+  {path: 'AdminLoginComponent', component: AdminLoginComponent},
+  {path: 'AdminCropApprovalComponent', component: AdminCropApprovalComponent},
+  {path: 'AdminApprovalComponent', component: AdminApprovalComponent},
+  {path: 'AdminAccountApprovalComponent', component: AdminAccountApprovalComponent},
+
   // {path: '**', component: ErrorComponent},
 ];
 
