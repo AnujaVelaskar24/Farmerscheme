15548579254d005e391af6e8a9db81f62f63ed45
AdityaS5 <aditya.sable511@gmail.com>
2021-04-08 18:21:58 +0530

    all files updated

--- app/footer/footer.component.spec.ts
+++ app/footer/footer.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdminApprovalComponent } from './admin-approval.component';
+import { FooterComponent } from './footer.component';
 
-describe('AdminApprovalComponent', () => {
-  let component: AdminApprovalComponent;
-  let fixture: ComponentFixture<AdminApprovalComponent>;
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminApprovalComponent ]
+      declarations: [ FooterComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminApprovalComponent);
+    fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
