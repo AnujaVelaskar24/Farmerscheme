efa85e17ea71b2436b9896a6ae1cb2b230181d95
aniketjadhav3422 <aniketjadhav3422@gmail.com>
2021-04-12 11:02:53 +0530

    validation

--- app/apply-bid/apply-bid.component.ts
+++ app/apply-bid/apply-bid.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-apply-bid',
