57c58626cba501ac990500a52c130fc6107cae6d
Anuja.Velaskar@lntinfotech.com <AnujaVelaskar@lntinfotech.com>
2021-04-15 09:04:18 +0530

    new desgins done in few pages

---
+++ app/sellrequest.ts
@@ -0,0 +1,10 @@
+export class Sellrequest {
+    request_id: number;
+    userid: number;
+    crop_id: number;
+    fertilizer_type: string;
+    quantity: number;
+    base_price: number;
+    crop_approval_status: boolean;
+    bid_approval_status: boolean;
+}
