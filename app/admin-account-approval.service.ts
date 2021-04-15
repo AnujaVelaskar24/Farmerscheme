d26d6b47d49dbcbbff56a14b1682507db64ae22a
AdityaS5 <aditya.sable511@gmail.com>
2021-04-15 23:41:22 +0530

    bid request, model class for class files, admin acc approval ui

--- app/admin-account-approval.service.ts
+++ app/admin-account-approval.service.ts
@@ -4,11 +4,10 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http
 import {  Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { Login } from './model/login';
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
+export class AdminAccountApprovalService {
   private apiServer = "http://localhost:59012/api";
   httpOptions = {
     headers:new HttpHeaders({
@@ -17,8 +16,11 @@ export class LoginService {
   }
 
   constructor(private httpClient: HttpClient) { }
- 
-login(login): Observable<any> {
-  return this.httpClient.post<any>(this.apiServer + '/login/', JSON.stringify(login), this.httpOptions)
+  
+Getuser_information(): Observable<any> {
+  return this.httpClient.get<any>(this.apiServer + '/adminaccountapproval/')
+}
+onapprove(userid,user): Observable<any> {
+  return this.httpClient.put<any>(this.apiServer + '/adminaccountapproval/', JSON.stringify(user), this.httpOptions)
 }
 }
