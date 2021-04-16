172b7df32191dba759a00167cb0e856922f90f7b
Anuja.Velaskar@lntinfotech.com <AnujaVelaskar@lntinfotech.com>
2021-04-16 15:46:41 +0530

    working on insurance claim

--- app/farmerhome/farmerhome.component.ts
+++ app/farmerhome/farmerhome.component.ts
@@ -14,7 +14,7 @@ export class FarmerhomeComponent implements OnInit {
   SellRequestForm = new FormGroup({
     croptype: new FormControl('',[Validators.required]),
     crop_id : new FormControl('',[Validators.required]),
-    fertilizer_type : new FormControl('',[Validators.required, Validators.pattern("^[a-zA-Z]+$")]),
+    fertilizer_type : new FormControl('',[Validators.required]),  //, Validators.pattern("^[a-zA-Z]+$")]),
     quantity : new FormControl('',[Validators.required, Validators.pattern("^[0-9]+$")]), 
     base_price : new FormControl('',[Validators.required, Validators.pattern("^[0-9]+$")]),
     userid: new FormControl(sessionStorage.getItem("userid")),
@@ -69,6 +69,10 @@ export class FarmerhomeComponent implements OnInit {
     });
   }
 
+  public selectedfertilizertype:string;
+  onfertilizertypeSelect(selectedfertilizertype){
+    console.log(selectedfertilizertype);
+  }
   
   onSubmit() {
     console.log(this.SellRequestForm.value, "Input data");
