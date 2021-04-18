314b6e111248d538dee2edd49d22098c724b3c83
AdityaS5 <aditya.sable511@gmail.com>
2021-04-19 00:37:29 +0530

    ohnoo

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -44,7 +44,6 @@ onSubmitLoginForm() {
     }
     else
     {
-      
       console.log("valid");
       //sessionStorage.setItem("username",this.LoginForm.value.username)
       sessionStorage.setItem("userid",res.userid)
