fc7f56b43d4b55fe206ebb572a031866fc926d8b
Anuja.Velaskar@lntinfotech.com <AnujaVelaskar@lntinfotech.com>
2021-04-20 00:32:18 +0530

    reset password

---
+++ app/resetpassword.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import {  Observable, throwError } from 'rxjs';
+import { Sellrequest } from './model/sellrequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResetpasswordService {
+
+  private apiServer = "http://localhost:59012/api";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  constructor(private httpClient: HttpClient) { }
+  
+  // resetpassword(): Observable<Sellrequest> {
+  //   return this.httpClient.put<Sellrequest>(this.apiServer + '/sellrequest/', JSON.stringify(sellrequest), this.httpOptions)
+  // }
+  // resetpassword(userid:number, password:string) {
+  //   this.httpClient.put(this.apiServer + '/resetpassword/', userid, password).subscribe(data => {
+  //     console.log(data);
+  //   });
+  // }
+  resetpassword(userid:number, password:string): Observable<any> {
+    return this.httpClient.post<any>(this.apiServer + '/resetpassword/', JSON.stringify(userid, password), this.httpOptions)
+  }
+}
